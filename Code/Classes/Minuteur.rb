fa9841aa4f1fab9dad3bb7f14d92d2ba38612ea1
TLOISON <theoloison.mails@gmail.com>
2018-03-02 09:22:11 +0100

    ajout timer

--- Code/Classes/Minuteur.rb
+++ Code/Classes/Minuteur.rb
@@ -1,17 +1,19 @@
 
 require 'thread'
-require 'gtk3'
+require 'gtk2'
 
 class Minuteur
 
 	attr_accessor :minuteur
+	attr_accessor :label
+	attr_accessor :malus
 	
 	#initialise le minuteur avec un temps (0 ou temps sauvegardé)
-	def initialize(temps,label)
+	def initialize(temps)
 		@minuteur=temps
 		@stop=1
 		@malus=0
-		@label=label
+		@label= Gtk::Label.new(to_s)
 
 	end
 	
@@ -29,29 +31,49 @@ class Minuteur
 	def setStop(bool)
 		@stop=bool
 	end
+	
+	def to_s
+			heures = @minuteur/3600
+			minutes = (@minuteur - (heures*3600)) / 60
+			secondes = @minuteur - (heures*3600) - (minutes*60)
+			
+			h = heures < 10 ? "0" + heures.to_s : heures.to_s
+			m = minutes < 10 ? "0" + minutes.to_s : minutes.to_s
+			s = secondes < 10 ? "0" + secondes.to_s : secondes.to_s
+			
+			return h + ":" + m + ":" + s
+	end
+
+	
+	def setLabel
+	
+			@label.set_text(to_s)
+			@label.show_all
+	end
 
 	#lance le minuteur
 	def start()
-		t1 = Thread.new{loop{@label.set_text(@minuteur);sleep(1);@minuteur+=1}}
 
-#		t2 = Thread.new{
-#			loop{
-#				puts " entrez un malus";
-#				malusTemps=gets;
-#				setMalus(malusTemps.to_i);
-#				@minuteur+=@malus;
-#				malustozero()
-#				puts "le minuteur doit il s'arreter ? (0 si oui 1 sinon)";
-#				arreter=gets;
-#				setStop(arreter.to_i)
-#			}
-#			
-#		}
+		t1 = Thread.new{loop{sleep(1);@minuteur+=1;setLabel}}
 
+=begin 	t2 = Thread.new{
+			loop{
+				set_text(" entrez un malus");
+				malusTemps=gets;
+				setMalus(malusTemps.to_i);
+				@minuteur+=@malus;
+				malustozero()
+				set_text("le minuteur doit il s'arreter ? (0 si oui 1 sinon)");
+				arreter=gets;
+				setStop(arreter.to_i)
+			}
+			
+	}
+=end
 		
 		
 		t1.priority=5;
- #       t2.priority=30;
+       # t2.priority=30;
         
 		
 		while(@stop != 0)do
@@ -60,7 +82,7 @@ class Minuteur
 
 
 		Thread.kill(t1)
-#		Thread.kill(t2)
+		#Thread.kill(t2)
 		
 		return @minuteur
 	end
@@ -68,3 +90,22 @@ class Minuteur
 	
 end
 
+
+Gtk.init 
+
+window = Gtk::Window.new
+
+m=Minuteur.new(0)
+
+
+window.add(m.label)
+
+
+
+
+#affiche la fenetre
+
+window.show_all
+
+Gtk.main
+m.start()
